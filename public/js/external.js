fa11c58a7d9f44bec4b0cc960a9402cc4a819591
Sakib Shaikh <sakibshaikh@Sakibs-MacBook-Air.local>
2015-10-02 13:26:59 -0500

    if_else excersises

--- public/js/external.js
+++ public/js/external.js
@@ -1,1 +1,20 @@
 console.log('Hello from External Javascript!')
+var raining=false;
+var foggy=false;
+var sunny=false;
+var cloudy=true;
+
+// if/else if/else example
+if (raining) {
+	console.log('Bring an umbrella brella brella ay ay');
+} else if (foggy) {
+	console.log('It is foggy');
+} else if (cloudy) {
+    // code here gets executed if condition2 evaluates to true
+} else if (sunny) {
+    // code here gets executed if condition2 evaluates to true
+} else {
+    // code here gets executed if neither condition1 nor condition2 evaluate to true
+}
+
+// note: you can leave off the trailing else if it isn't needed
---
+++ public/js/if_else.js
@@ -0,0 +1,49 @@
+// ignore these lines for now
+// just know that the variable 'color' will end up with a random value from the colors array
+var colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+var color = colors[Math.floor(Math.random()*colors.length)];
+
+var favorite = 'green'; // TODO: change this to your favorite color from the list
+
+// TODO: Create a block of if/else statements to check for every color except indigo and violet.
+// if/else example
+color;
+
+if (color=="red") {
+console.log("Red is the color of an apple");
+}
+else {
+	if (color=="orange"){
+	console.log("Orange is the color of an orange");
+	}
+		else{
+			if (color=="yellow") {
+				console.log("Yellow is the color of lemons");
+				}
+					else{
+						if (color=="green") {
+							console.log ("Green is the color of leaves");
+							}
+								else{
+									if (color=="blue"){
+										console.log ("Blue is the color of the sky");
+										}
+	}
+			}
+						}
+									}
+
+// the above if/else can be re-written as:
+(color == "violet") ? "Operation was successful." : "Oops, something went wrong.";
+(color == "indigo") ? "Operation was successful." : "Oops, something went wrong.";
+
+
+
+// TODO: When a color is encountered log a message that tells the color, and an object of that color.
+//       Example: Blue is the color of the sky.
+
+// TODO: Have a final else that will catch indigo and violet.
+// TODO: In the else, log: I do not know anything by that color.
+
+// TODO: Using the ternary operator, conditionally log a statement that
+//       says whether the random color matches your favorite color.
